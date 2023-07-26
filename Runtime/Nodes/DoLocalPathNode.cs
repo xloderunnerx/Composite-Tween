421fb7c22b09317b46be03e70be9c5c046dcffb0
Richard <xloderunnerx@gmail.com>
2023-07-26 17:46:09 +0300

    DoLocal Path.

---
+++ Runtime/Nodes/DoLocalPathNode.cs
@@ -0,0 +1,23 @@
+using CompositeTween.Core;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace CompositeTween.Node
+{
+    public class DoLocalPathNode : TweenNode
+    {
+        [SerializeField] private RectTransform transform;
+        [SerializeField] private List<RectTransform> path;
+        [SerializeField] private PathType pathType;
+
+        public override Sequence GetSequence()
+        {
+            var sequence = base.GetSequence();
+            sequence.Insert(insertTime, transform.DOLocalPath(path.Select(pathNode => pathNode.transform.localPosition).ToArray(), duration, pathType));
+            return sequence;
+        }
+    }
+}
