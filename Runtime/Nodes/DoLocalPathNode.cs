27cb7427273cb8ca5283ac7102efbdd7350df0fa
Richard <xloderunnerx@gmail.com>
2023-07-27 17:33:29 +0300

    DoPath nodes have loops now.

--- Runtime/Nodes/DoLocalPathNode.cs
+++ Runtime/Nodes/DoLocalPathNode.cs
@@ -12,10 +12,12 @@ namespace CompositeTween.Node
         [SerializeField] private RectTransform transform;
         [SerializeField] private List<RectTransform> path;
         [SerializeField] private PathType pathType;
+        [SerializeField] private int loops;
 
         public override Sequence GetSequence()
         {
             var sequence = base.GetSequence();
+            sequence.SetLoops(loops);
             sequence.Insert(insertTime, transform.DOLocalPath(path.Select(pathNode => pathNode.transform.localPosition).ToArray(), duration, pathType));
             return sequence;
         }
