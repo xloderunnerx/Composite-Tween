98756fe5ad974a94cea3a7416f78a717a6fe3d09
Richard <xloderunnerx@gmail.com>
2023-08-10 16:03:25 +0300

    DoWaitNode.

---
+++ Runtime/Nodes/DoWaitNode.cs
@@ -0,0 +1,23 @@
+﻿using CompositeTween.Core;
+using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CompositeTween.Node
+{
+    [System.Serializable]
+    public class DoWaitNode : TweenNode
+    {
+        public override Sequence GetSequence()
+        {
+            var sequence = base.GetSequence();
+            var timer = 0;
+            sequence.Insert(insertTime, DOTween.To(() => timer, x => timer = x, 0, duration));
+            return sequence;
+        }
+    }
+}
